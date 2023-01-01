69e79d9416f5f144565527323de2d0855503c75f
myunus-info <myunus_info@yahoo.com>
2023-01-01 18:20:33 +0600

    Signup and Login Complete

--- src/modules/user/user.model.js
+++ src/modules/user/user.model.js
@@ -1,10 +1,38 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-  name: {
+  username: {
     type: String,
-    required: true,
+    unique: true,
+    required: [true, 'Please provide username'],
+    validate: {
+      validator: validator.isEmail,
+      message: 'Username must be a valid email',
+    },
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide password'],
+    minlength: 6,
+  },
+  role: {
+    type: String,
+    enum: ['buyer', 'seller'],
+    default: 'buyer',
   },
 });
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (canditatePassword) {
+  return await bcrypt.compare(canditatePassword, this.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
--- src/modules/user/user.service.js
+++ src/modules/user/user.service.js
@@ -3,12 +3,19 @@ const jwt = require('jsonwebtoken');
 const nodeCache = require(path.join(process.cwd(), 'src/config/lib/nodecache'));
 
 const generateAccessToken = user => {
-  const accessToken = jwt.sign({ id: user.id }, nodeCache.getValue('JWT_SECRET'), {
+  const accessToken = jwt.sign({ id: user._id }, nodeCache.getValue('JWT_SECRET'), {
     expiresIn: nodeCache.getValue('JWT_EXPIRES_IN'),
-    issuer: user.id.toString(),
+    issuer: user._id.toString(),
   });
 
   return accessToken;
 };
 
-module.exports = { generateAccessToken };
+const generateTokenUser = user => {
+  return { name: user.username, userId: user._id, role: user.role };
+};
+
+module.exports = {
+  generateAccessToken,
+  generateTokenUser,
+};
